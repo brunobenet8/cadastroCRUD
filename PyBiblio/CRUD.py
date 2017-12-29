deb20a9e514b10950b684370eae9ff4414c31466
brunobenet8 <bruno.rafaelbenet@gmail.com>
2017-12-29 12:05:39 -0200

    Initial commit

---
+++ PyBiblio/CRUD.py
@@ -0,0 +1,61 @@
+import sqlite3
+
+
+def inserir(nome, endereco, obs):
+    conn = sqlite3.connect('dbContatos')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO Lista(Nome, Endereco, Obs) values(?,?,?)', (nome, endereco, obs))
+    conn.commit()
+    conn.close()
+    return 'Cadastrado com Sucesso!'
+
+
+def listar(id):
+    conn = sqlite3.connect('dbContatos')
+    cursor = conn.cursor()
+    cursor.execute('select * from Lista where ID = ?', (str(id)))
+    for linha in cursor.fetchall():
+        return linha
+    conn.close()
+
+
+def deletar(id):
+    conn = sqlite3.connect('dbContatos')
+    cursor = conn.cursor()
+    cursor.execute('DELETE from Lista where ID = ?', (str(id)))
+    conn.commit()
+    conn.close
+    return 'Cadastro deletado com sucesso!'
+
+
+def alterar(id, nome, endereco, obs):
+    conn = sqlite3.connect('dbContatos')
+    cursor = conn.cursor()
+    cursor.execute('UPDATE Lista SET Nome = ?, Endereco = ?, Obs = ? where ID = ?', (nome, endereco, obs, id))
+    conn.commit()
+    conn.close()
+    return 'Dados alterados com Sucesso!'
+
+
+def carregar():
+    conn = sqlite3.connect('dbContatos')
+    cursor = conn.cursor()
+    cursor.execute('select * from Lista')
+    return cursor.fetchall()
+    conn.close()
+
+
+def buscar(termo, tipo):
+    if(termo == 0):
+        sql = "select * from Lista where Id like ?"
+    elif(termo == 1):
+        sql = "select * from Lista where Nome like ?"
+    elif(termo == 2):
+        sql = "select * from Lista where Endereco like ?"
+    elif(termo == 3):
+        sql = "select * from Lista where Obs like ?"
+    conn = sqlite3.connect('dbContatos')
+    cursor = conn.cursor()
+    cursor.execute(sql, ('%'+tipo+'%',))
+    return cursor.fetchall()
+    conn.close
---
+++ PyBiblio/TelaMenu.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from PyQt4 import QtCore, QtGui
+import TelaCadastro
+import TelaBusca
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(320, 240)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.verticalLayoutWidget = QtGui.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 160, 80))
+        self.verticalLayoutWidget.setObjectName(_fromUtf8("verticalLayoutWidget"))
+        self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.pushButton = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.verticalLayout.addWidget(self.pushButton)
+        self.pushButton_2 = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.verticalLayout.addWidget(self.pushButton_2)
+        self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(160, 190, 151, 20))
+        self.label.setObjectName(_fromUtf8("label"))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.pushButton.clicked.connect(self.telacadastro)
+        self.pushButton_2.clicked.connect(self.telabusca)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "Menu", None))
+        self.pushButton.setText(_translate("MainWindow", "Cadastro", None))
+        self.pushButton_2.setText(_translate("MainWindow", "Pesquisa", None))
+        self.label.setText(_translate("MainWindow", "Cadastro de Contatos", None))
+
+    def telacadastro(self):
+        Dialog = QtGui.QDialog()
+        ui = TelaCadastro.Ui_Dialog()
+        ui.setupUi(Dialog)
+        Dialog.show()
+        Dialog.exec_()
+
+    def telabusca(self):
+        Dialog = QtGui.QDialog()
+        ui = TelaBusca.Ui_Dialog()
+        ui.setupUi(Dialog)
+        ui.preenche()
+        Dialog.show()
+        Dialog.exec_()
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = QtGui.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
