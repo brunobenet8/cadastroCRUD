e71cd8fd4a7ea059f4af6881d65f73048dfd542f
brunobenet8 <bruno.rafaelbenet@gmail.com>
2018-01-08 20:08:34 -0200

    Change all

--- PyBiblio/TelaMenu.py
+++ PyBiblio/TelaMenu.py
@@ -1,82 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from PyQt4 import QtCore, QtGui
-import TelaCadastro
-import TelaBusca
+import sys
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(320, 240)
-        self.centralwidget = QtGui.QWidget(MainWindow)
-        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.verticalLayoutWidget = QtGui.QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 160, 80))
-        self.verticalLayoutWidget.setObjectName(_fromUtf8("verticalLayoutWidget"))
-        self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.pushButton = QtGui.QPushButton(self.verticalLayoutWidget)
-        self.pushButton.setObjectName(_fromUtf8("pushButton"))
-        self.verticalLayout.addWidget(self.pushButton)
-        self.pushButton_2 = QtGui.QPushButton(self.verticalLayoutWidget)
-        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
-        self.verticalLayout.addWidget(self.pushButton_2)
-        self.label = QtGui.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(160, 190, 151, 20))
-        self.label.setObjectName(_fromUtf8("label"))
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QtGui.QStatusBar(MainWindow)
-        self.statusbar.setObjectName(_fromUtf8("statusbar"))
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-        self.pushButton.clicked.connect(self.telacadastro)
-        self.pushButton_2.clicked.connect(self.telabusca)
-
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(_translate("MainWindow", "Menu", None))
-        self.pushButton.setText(_translate("MainWindow", "Cadastro", None))
-        self.pushButton_2.setText(_translate("MainWindow", "Pesquisa", None))
-        self.label.setText(_translate("MainWindow", "Cadastro de Contatos", None))
-
-    def telacadastro(self):
-        Dialog = QtGui.QDialog()
-        ui = TelaCadastro.Ui_Dialog()
-        ui.setupUi(Dialog)
-        Dialog.show()
-        Dialog.exec_()
-
-    def telabusca(self):
-        Dialog = QtGui.QDialog()
-        ui = TelaBusca.Ui_Dialog()
-        ui.setupUi(Dialog)
-        ui.preenche()
-        Dialog.show()
-        Dialog.exec_()
+class Application(QWidget):
+    def __init__(self):
+        self.w = QMainWindow()
+        self.menubar = self.w.menuBar()
+        self.cadastro = self.menubar.addMenu('Cadastros')
+        self.cadastro.addAction('Cadastro')
+        self.cadastro.addAction('Pesquisa')
+        self.sb = QStatusBar()
+        self.l1 = QLabel('Sistema de Cadastro de Contatos')
+        self.sb.addWidget(self.l1)
+        self.w.setStatusBar(self.sb)
+        self.w.resize(320, 240)
+        self.w.setWindowTitle('..:: Contatos ::..')
+        self.w.show()
 
 
-if __name__ == "__main__":
-    import sys
-    app = QtGui.QApplication(sys.argv)
-    MainWindow = QtGui.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
-
+app = QApplication(sys.argv)
+a = Application()
+sys.exit(app.exec_())
