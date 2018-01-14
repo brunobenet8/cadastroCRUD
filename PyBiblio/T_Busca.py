b565030ceff996060bfb2e67d2bc88217cf6b448
brunobenet8 <bruno.rafaelbenet@gmail.com>
2018-01-14 15:20:33 -0200

    Arrumado erro de identação

--- PyBiblio/T_Busca.py
+++ PyBiblio/T_Busca.py
@@ -52,15 +52,15 @@ class Application(QWidget):
         self.w.show()
 
         def buscar(self):
-        self.tb.setRowCount(0)
-        resultado = CRUD.buscar(self.cb.currentIndex(), str(self.t1.text()))
-        for linha in range(len(resultado)):
-            self.tb.insertRow(linha)
-            self.tb.setItem(linha, 0,
-                QTableWidgetItem(str(resultado[linha][0])))
-            self.tb.setItem(linha, 1,
-                QTableWidgetItem(str(resultado[linha][1])))
-            self.tb.setItem(linha, 2,
-                QTableWidgetItem(str(resultado[linha][2])))
-            self.tb.setItem(linha, 3,
-                QTableWidgetItem(str(resultado[linha][3])))
+            self.tb.setRowCount(0)
+            resultado = CRUD.buscar(self.cb.currentIndex(), str(self.t1.text()))
+            for linha in range(len(resultado)):
+                self.tb.insertRow(linha)
+                self.tb.setItem(linha, 0,
+                    QTableWidgetItem(str(resultado[linha][0])))
+                self.tb.setItem(linha, 1,
+                    QTableWidgetItem(str(resultado[linha][1])))
+                self.tb.setItem(linha, 2,
+                    QTableWidgetItem(str(resultado[linha][2])))
+                self.tb.setItem(linha, 3,
+                    QTableWidgetItem(str(resultado[linha][3])))
